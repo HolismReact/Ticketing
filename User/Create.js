c9ec8b934dd0e86913895ce8c0bd5496f30745d3
Saeed Nemati <sn.1361@gmail.com>
2021-11-23 14:37:44 +0330

    committed by script

---
+++ User/Create.js
@@ -0,0 +1,30 @@
+import { Form, Text, Enum, LongText } from '@Form';
+
+const fields =
+    <>
+        <Text
+            column='title'
+            placeholder='Title'
+            required='Please write the title.'
+        />
+        <Enum
+            column='priorityId'
+            entity='priority'
+            placeholder='Priority'
+            required='Please choose the priority for this ticket. Choose less importance if it is less urgent.'
+        />
+        <LongText
+            column='body'
+            placeholder='Please describe the problem!'
+            required='We need to know the problem to be able to help.'
+        />
+    </>
+
+const CreateTicket = (props) => {
+    return <Form
+        entity='ticket'
+        inputs={fields}
+    />
+}
+
+export default CreateTicket;
