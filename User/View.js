71de693a5dd27d71c6f7dc560c3742d53862304b
Saeed Nemati <sn.1361@gmail.com>
2021-11-29 12:08:17 +0330

    committed by script

--- User/View.js
+++ User/View.js
@@ -1,10 +1,35 @@
-import { PagePadding } from '@Panel';
+import { PagePadding, get, app } from '@Panel';
+import React, { useState, useEffect } from 'react';
 
 const ViewTicket = () => {
+
+    const [progress, setProgress] = useState(false);
+    const [ticket, setTicket] = useState();
+
+    const loadTicket = () => {
+        setProgress(true);
+        get('/ticket/view')
+            .then((data) => {
+                setProgress(false);
+                setTicket(data);
+            }, (error) => {
+                setProgress(false);
+                app.error(error);
+            });
+    }
+
+    useEffect(() => {
+        loadTicket();
+        app.emit(app.componentLoaded, {pageTitle: 'View ticket '});
+    }, []);
+
     return <div
         className={PagePadding}
     >
-        Viewing ticket
+        <div className="ticket">Ticket</div>
+        <div className="posts">
+
+        </div>
     </div>
 }
 
