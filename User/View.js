3c2e54e00027ad79109c5acf8a066fdd4b63990b
Saeed Nemati <sn.1361@gmail.com>
2021-11-29 18:27:44 +0330

    committed by script

--- User/View.js
+++ User/View.js
@@ -1,14 +1,26 @@
-import { PagePadding, get, app } from '@Panel';
+import { PagePadding, get, post, app } from '@Panel';
+import { LongText } from '@Form';
 import React, { useState, useEffect } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import Button from '@material-ui/core/Button';
 
 const ViewTicket = () => {
 
     const [progress, setProgress] = useState(false);
+    const [submitting, setSubmitting] = useState(false);
     const [ticket, setTicket] = useState();
+    const [showForm, setShowForm] = useState(false);
+    const [isValid, setIsValid] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const { ticketId } = app.parseQuery();
 
     const loadTicket = () => {
         setProgress(true);
-        get('/ticket/view')
+        get(`/ticket/view?ticketId=${ticketId}`)
             .then((data) => {
                 setProgress(false);
                 setTicket(data);
@@ -18,18 +30,136 @@ const ViewTicket = () => {
             });
     }
 
+    const handleSubmit = () => {
+        setSubmitting(true);
+        post(`/ticket/addUserResponse`, { ticketId: ticketId * 1, message })
+            .then((post) => {
+                setSubmitting(false);
+                setShowForm(false);
+                loadTicket();
+            }, (error) => {
+                setSubmitting(false);
+                app.error(error);
+            })
+    }
+
     useEffect(() => {
         loadTicket();
-        app.emit(app.componentLoaded, {pageTitle: 'View ticket '});
+        app.emit(app.componentLoaded, { pageTitle: 'View ticket ' });
     }, []);
+
+    useEffect(() => {
+        if (message) {
+            setIsValid(true);
+        }
+        else {
+            setIsValid(false);
+        }
+    }, [message]);
+
+    useEffect(() => {
+        if (showForm) {
+            document.querySelector('#postMessage').focus();
+        }
+    }, [showForm]);
+
+    const Ticket = <div className="ticket ">
+        <div className="bg-green-200 md:rounded-lg p-6">
+            <div>Ticket #{ticket?.ticket?.id}</div>
+            <div>{ticket?.ticket?.title}</div>
+            <div>Created at:</div>
+            <div>{ticket?.ticket?.utcDate}</div>
+            <div>Last updated at:</div>
+            <div>{ticket?.ticket?.latestPostUtcDate}</div>
+            <div>Importance:</div>
+            <div>{ticket?.ticket?.priorityKey}</div>
+            <div>State:</div>
+            <div>{ticket?.ticket?.titleizedStateKey}</div>
+        </div>
+    </div>
+
+    const Form = <div className="mb-6">
+        <Collapse in={showForm}>
+            <div className="bg-white p-6 ">
+                {/* <LongText
+                    column="html"
+                    placeholder="Please write your response here."
+                /> */}
+                <textarea
+                    id='postMessage'
+                    className="w-full p-3"
+                    onChange={(e) => setMessage(e.target.value)}
+                    rows="10"
+                    disabled={submitting}
+                >
+
+                </textarea>
+                <div className="mt-3 float-right ">
+                    {
+                        submitting
+                            ?
+                            <CircularProgress />
+                            :
+                            null
+                    }
+                    <Button
+                        variant="outlined"
+                        className={(isValid ? " bg-green-200" : "")}
+                        onClick={handleSubmit}
+                        disabled={!isValid}
+                    >
+                        {app.t('Save')}
+                    </Button>
+                </div>
+                <div className="clear-both"></div>
+            </div>
+        </Collapse>
+        {
+            showForm
+                ?
+                <div className="w-full pt-6 cursor-pointer" onClick={() => setShowForm(false)}>
+                    <ExpandLessIcon />
+                    <span className="text-sm mx-3 lowercase font-light tracking-wider">Hide form</span>
+                </div>
+                :
+                <div className="w-full cursor-pointer" onClick={() => setShowForm(true)}>
+                    <ExpandMoreIcon />
+                    <span className="text-sm mx-3 lowercase font-light tracking-wider">Show form</span>
+                </div>
+        }
+    </div>
+
+    const Posts = <div className="posts lg:col-span-3 lg:row-start-1">
+        {Form}
+        {
+            ticket?.posts?.map((post, index) => <div key={post.id} className={
+                "post bg-white p-6 border-b whitespace-pre-wrap "
+                + (index === 0 ? " rounded-tl-lg rounded-tr-lg" : "")
+                + (index === ticket.posts.length - 1 ? " rounded-bl-lg rounded-br-lg " : "")
+                + (post.isSystemPost ? " bg-gray-300 " : "")
+            }>
+                <div>
+                    <span>{post.utcDate}</span>
+                    <span>{post.isSystemPost ? "User" : "System"}</span>
+                </div>
+                {post.relatedItems.html}
+            </div>)
+        }
+    </div>
 
     return <div
         className={PagePadding}
     >
-        <div className="ticket">Ticket</div>
-        <div className="posts">
-
-        </div>
+        {
+            progress
+                ?
+                <CircularProgress />
+                :
+                <div className="grid grid-cols-1 gap-6 lg:grid-cols-4 ">
+                    {Ticket}
+                    {Posts}
+                </div>
+        }
     </div>
 }
 
